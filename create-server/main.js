72ba681d0a46db523d8c208acbb9e7f4c9abd2df
Mridul-06-stack <24bec066@nith.ac.in>
2025-12-20 22:54:03 +0530

    hwo logs are saved

--- create-server/main.js
+++ create-server/main.js
@@ -1,7 +1,13 @@
 const http=require("http");
+const  fs=require("fs");
 const server=http.createServer((req,res) => {
+    console.log("Request received");
+    fs.appendFile("log.txt",`request received at ${Date.now().toString()}\n`, (err) => {
+        if (err) throw err;
+    });
     res.end("Hello from my first Node.js server!");
 })
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
+    
 });
