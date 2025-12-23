c277e7dac95d3265ebbb01df15a9aa311c8d69b1
Mridul-06-stack <24bec066@nith.ac.in>
2025-12-23 14:40:42 +0530

    add mockdata

--- project-1/index.js
+++ project-1/index.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const app=express();
 const port=3000;  
+const users=require("./MOCK_DATA.json")
 app.get("/",(req,res)=>{
     res.send("Hello World!");
 });
+app.get("/users",(req,res)=>{
+    res.json(users);
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
