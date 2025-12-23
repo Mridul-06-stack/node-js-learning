e89a7816c9f9f098b42ee03782e5cd8a2469567f
Mridul-06-stack <24bec066@nith.ac.in>
2025-12-23 17:54:51 +0530

    add paramters route

--- project-1/index.js
+++ project-1/index.js
@@ -5,9 +5,24 @@ const users=require("./MOCK_DATA.json")
 app.get("/",(req,res)=>{
     res.send("Hello World!");
 });
-app.get("/users",(req,res)=>{
+app.get("/api/users",(req,res)=>{
     res.json(users);
 });
+app.get("/users",(req,res)=>{
+    res.send(
+        
+        `
+        <h1>Users List</h1>
+        <ol>
+        ${users.map(user=>{return `<li>${user.first_name}</li>`}).join('')}
+        </ol>`
+    );
+});
+app.get("/users/:id",(req,res)=>{
+    const id= req.params.id;
+    const user= users.find(u=>u.id==id);
+    res.json(user);
+})
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
