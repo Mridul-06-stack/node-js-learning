d1933e9c61beec0409692b171cd08205e53752f2
Mridul-06-stack <24bec066@nith.ac.in>
2025-12-24 17:43:29 +0530

    add post req

--- project-1/index.js
+++ project-1/index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const app=express();
 const port=3000;  
+const fs=require('fs'); 
 const users=require("./MOCK_DATA.json")
+app.use(exxpress.urlencoded({extended:false}));
 app.get("/",(req,res)=>{
     res.send("Hello World!");
 });
@@ -23,6 +25,17 @@ app.get("/users/:id",(req,res)=>{
     const user= users.find(u=>u.id==id);
     res.json(user);
 })
+app.post("/api/users",(req,res)=>{
+    const mess=req.body;
+    users.push(...mess,users.length+1);
+
+        fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>{
+            if(err){
+                console.log("Error writing file",err);
+            }
+        });
+    res.send("POST request to the users");
+});
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
